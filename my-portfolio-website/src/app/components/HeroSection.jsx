0df8fb0991b7c9787530591d31824401285316c0
Russel-T <russel.abraham123@gmail.com>
2024-06-13 13:06:01 -0500

    herosection edit

--- my-portfolio-website/src/app/components/HeroSection.jsx
+++ my-portfolio-website/src/app/components/HeroSection.jsx
@@ -4,21 +4,21 @@ const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+        <div className="col-span-7 place self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl  lg:text-6xl font-extrabold">
             Hello, I'm Russel
           </h1>
-          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ipsa
             id, laborum quasi distinctio possimus ex expedita, accusamus
             suscipit aspernatur quo aliquid sequi. Nihil vero dicta at molestias
             ex beatae.
           </p>
           <div>
-            <button className="px-6 py-3 bg-[#00df9a] rounded mr-4 hover:bg-[#00cf9a] ">
+            <button className="px-6 py-3 w-full sm:w-fit bg-[#00df9a] mr-4 hover:bg-[#00cf9a] ">
               Hire Me
             </button>
-            <button className="px-6 py-3 bg-transparent rounded text-[#00df9a] border border-[#00df9a] hover:bg-[#00df9a] hover:text-black mt-3">
+            <button className="px-6 py-3  w-full sm:w-fit bg-transparent rounded text-[#00df9a] border border-[#00df9a] hover:bg-[#00df9a] hover:text-black mt-3">
               My Resume
             </button>
           </div>
