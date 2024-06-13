ca3ac804437c974579ba4a037a551a7fc2ce4d8c
Russel-T <russel.abraham123@gmail.com>
2024-06-13 01:31:09 -0500

    Currently on: 17:20

--- my-portfolio-website/src/app/page.js
+++ my-portfolio-website/src/app/page.js
@@ -2,8 +2,10 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-black container mx-auto px-12 py-4">
-      <HeroSection />
+    <main className="flex min-h-screen bg-black">
+      <div class="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
