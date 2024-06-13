726ca803d2d86d05b10aa75d860c648b426e249a
Russel-T <russel.abraham123@gmail.com>
2024-06-13 01:28:21 -0500

    HeroSection Edit

--- my-portfolio-website/src/app/components/HeroSection.jsx
+++ my-portfolio-website/src/app/components/HeroSection.jsx
@@ -4,29 +4,32 @@ const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7">
+        <div className="col-span-7 place self-center">
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
             Hello, I'm Russel
           </h1>
           <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
-            I'm a full-stack developer
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ipsa
+            id, laborum quasi distinctio possimus ex expedita, accusamus
+            suscipit aspernatur quo aliquid sequi. Nihil vero dicta at molestias
+            ex beatae.
           </p>
           <div>
             <button className="px-6 py-3 bg-[#00df9a] rounded mr-4 hover:bg-[#00cf9a] ">
               Hire Me
             </button>
-            <button className="px-6 py-3 bg-transparent rounded mr-4 text-[#00df9a] border border-[#00df9a] hover:bg-[#00df9a] hover:text-black">
+            <button className="px-6 py-3 bg-transparent rounded text-[#00df9a] border border-[#00df9a] hover:bg-[#00df9a] hover:text-black mt-3">
               My Resume
             </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/memoji.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={500}
+              width={300}
               height={300}
             />
           </div>
