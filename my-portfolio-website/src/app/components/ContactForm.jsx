e62854c8a76a68c54eea12e3d6949ceec50840df
Russel-T <russel.abraham123@gmail.com>
2024-06-26 14:13:23 -0500

    adding email section

---
+++ my-portfolio-website/src/app/components/ContactForm.jsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_9udy9ou", "template_qm1igot", form.current, {
+        publicKey: "-rK2dGng-ABYedwAi",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+export default ContactForm;
+// Reference: https://youtu.be/bMq2riFCF90
