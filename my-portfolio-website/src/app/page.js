c20599e15edc74d38b630eb5bc386cb3c2611c1c
Russel-T <russel.abraham123@gmail.com>
2024-06-27 17:43:57 -0500

    removed navbar and navlink

--- my-portfolio-website/src/app/page.js
+++ my-portfolio-website/src/app/page.js
@@ -7,8 +7,15 @@ import ContactForm from "./components/ContactForm";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black">
-      <Navbar />
-      <div class="container mt-24 mx-auto px-12 py-4">
+      {/* <Navbar /> */}
+      {/* <div class="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+        <AboutSection />
+        <ProjectsSection />
+        <ContactForm />
+      </div> */}
+
+      <div class="container mt-10 mx-auto px-12">
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
