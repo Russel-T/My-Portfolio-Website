75f95eb9ae3f4f6b1010d46265ae2121ef013250
Russel-T <russel.abraham123@gmail.com>
2024-07-03 14:52:43 -0500

    revision

--- my-portfolio-website/src/app/components/AboutSection.jsx
+++ my-portfolio-website/src/app/components/AboutSection.jsx
@@ -76,11 +76,7 @@ const AboutSection = () => {
   return (
     <section className="text-white">
       <div className="gap-8 items-center py-8 px-4 xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-16">
-        <Image
-          src="/images/roozbeh-eslami-YlVjrBkfXt8-unsplash.jpg"
-          width={900}
-          height={900}
-        />
+        <Image src="/images/Computer Image.jpg" width={900} height={900} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-white text-base lg:text-lg">
